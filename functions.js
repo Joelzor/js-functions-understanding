8b2a3859795ad190e60c8a88f2a58eee99a381c6
Joel Watkins <eck087@yahoo.co.uk>
2022-09-14 14:39:15 +0100

    q6-9 done

--- functions.js
+++ functions.js
@@ -41,14 +41,64 @@
   //because num is not called in the function call only the value of add will have changed through the function. so the resulting will be 1.
 
 
-  function myFunction(num, num1) {
-    console.log(num1)
-  }
+  // function myFunction(num, num1) {
+  //   console.log(num1)
+  // }
   
-  let num = 10
-  let num1 = 2
+  // let num = 10
+  // let num1 = 2
   
-  console.log(myFunction(num))
+  // console.log(myFunction(num))
   
 // I know that this function will return undefined as there is no return. 
   //the function also expects two arguments but there is only being one passed through it
+
+// function myFunction(num, num1) {
+//   console.log(num1)
+// }
+  
+// let num = 10
+// let num1 = 2
+  
+// myFunction(num1, num)
+
+// This function will log 10 to the console because we pass num(10) as the second argument in the function call. The function
+// only logs the second argument 
+
+// let counter = 1
+
+// function myFunction() {
+//   counter++
+//   return counter
+// }
+
+// myFunction() // 2
+// const num = myFunction() // 3
+// console.log(num)
+
+// The function adds 1 to the counter and then returns the value. We call it twice so we add 1 each time.
+
+// function myFunction(num1, num2) {
+//   return num1 + num2
+// }
+
+// const num1 = 10
+// const num2 = 1
+// const num3 = 4
+
+// const result = myFunction(num3, num1)
+// console.log(result)
+
+// The function adds num1 and num2 together. We pass it 4 (num3) and 10 (num1), resulting in 14!
+
+// function myFunction(num1, num2) {
+//   console.log(num3)
+// }
+
+// const num1 = 10
+// const num2 = 1
+// const num3 = 20
+
+// myFunction(num3, num1)
+
+// Logs num3, which isn't one of the parameters. The function can access the global variable num3 (meaning it is declared outside a function block). 
