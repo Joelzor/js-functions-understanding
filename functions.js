def42aba56b286ed3d860c90f0e560f3f0af5d28
Joel Watkins <eck087@yahoo.co.uk>
2022-09-14 12:21:45 +0100

    q1 done

--- functions.js
+++ functions.js
@@ -1,1 +1,9 @@
-'hello'
+function myFunction(num1, num2) {
+  return num1+num2
+}
+
+const result = myFunction(5,5)
+console.log(result)
+
+// This function takes two parameters and returns the sum of both of them. 
+// In the example, 10 will be stored in the result variable 
